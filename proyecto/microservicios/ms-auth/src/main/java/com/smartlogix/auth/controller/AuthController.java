5cd922f1ff941a23a6373f97cb27a560449f170d
Maximiliano A. Madrid Fuentes <maxi.madrid@duocuc.cl>
2026-05-19 09:26:48 +0000

    v0.5

--- proyecto/microservicios/ms-auth/src/main/java/com/smartlogix/auth/controller/AuthController.java
+++ proyecto/microservicios/ms-auth/src/main/java/com/smartlogix/auth/controller/AuthController.java
@@ -21,4 +21,18 @@ public class AuthController {
 
         return ResponseEntity.ok("Usuario registrado correctamente");
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+    User user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+    if (!passwordEncoder.matches(request.getContraseña(), user.getContraseña())) {
+        return ResponseEntity.status(401).body("Credenciales incorrectas");
+    }
+
+    String token = jwtService.generateToken(user.getEmail());
+    return ResponseEntity.ok(new AuthResponse(token));
+    }
 }
+
