d5b4f46c54ce98ca5fbe706bd3a51b68087ffb57
Maximiliano A. Madrid Fuentes <maxi.madrid@duocuc.cl>
2026-05-20 17:44:23 +0000

    v6

--- prueba-smartlogix-main/proyecto/microservicios/ms-auth/src/main/java/com/smartlogix/auth/entity/User.java
+++ prueba-smartlogix-main/proyecto/microservicios/ms-auth/src/main/java/com/smartlogix/auth/entity/User.java
@@ -2,6 +2,8 @@ package com.smartlogix.auth.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
