65792f73e559b67587ae7bdd15135360e12d2135
Maximiliano A. Madrid Fuentes <maxi.madrid@duocuc.cl>
2026-07-10 03:51:58 -0400

    v0.14

--- prueba-smartlogix-master/prueba-smartlogix-main/proyecto/api-gateway/src/main/java/com/smartlogix/gateway/ApiGatewayApplication.java
+++ prueba-smartlogix-master/prueba-smartlogix-main/proyecto/api-gateway/src/main/java/com/smartlogix/gateway/ApiGatewayApplication.java
@@ -2,6 +2,14 @@ package com.smartlogix.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +18,17 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
 }
